062f32ec9703a6440643a3f4dec1ea3fd8802da1
ismayil10 <ismayiltg-apa101@code.edu.az>
2025-11-14 00:33:05 +0400

    Fix the bug

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -4,97 +4,103 @@ using Repository.Implementations;
 using Service.Helpers;
 using Service.Implementations;
 using System.Linq;
+class Program {
+    static void Main(string[] args) {
 
-IGroupRepository groupRepository = new GroupRepository();
-IStudentRepository studentRepository = new StudentRepository();
-IGroupService groupService = new GroupService(groupRepository, studentRepository);
-IStudentService studentService = new StudentService(studentRepository, groupRepository);
+        IGroupRepository groupRepository = new GroupRepository();
+        IStudentRepository studentRepository = new StudentRepository();
+        IGroupService groupService = new GroupService(groupRepository, studentRepository);
+        IStudentService studentService = new StudentService(studentRepository, groupRepository);
 
-Helpers.DisplayInfo("Welcome to Course Management Console Application!");
+        Helpers.DisplayInfo("Welcome to Course Management Console Application!");
 
-var exitRequested = false;
-while (!exitRequested)
-{
-    var handler = new MenuHandler(groupService, studentService);
-    handler.PrintMenu();
-    var choice = Helpers.ReadInput("Enter your choice (0 - Exit)");
-    
-    if (choice == null) continue; // ESC pressed - show menu again
-
-    try
-    {
-        switch (choice)
+        var exitRequested = false;
+        while (!exitRequested)
         {
-            case "0":
-                exitRequested = true;
-                Helpers.DisplayMessage("Program is stopping. Thank you!", ConsoleColor.Yellow);
-                break;
+            var handler = new MenuHandler(groupService, studentService);
+            handler.PrintMenu();
+            var choice = Helpers.ReadInput("Enter your choice (0 - Exit)");
 
-            case "1":
-                handler.CreateGroup();
-                Helpers.PlaySound("Succes.wav");
-                break;
-            case "2":
-                handler.UpdateGroup();
-                break;
-            case "3":
-                handler.DeleteGroup();
-                break;
-            case "4":
-                handler.GetGroupById();
-                break;
-            case "5":
-                handler.GetGroupsByTeacher();
-                break;
-            case "6":
-                handler.GetGroupsByRoom();
-                break;
-            case "7":
-                handler.GetAllGroups();
-                break;
+            if (choice == null) continue;
 
-            case "8":
-                handler.CreateStudent();
-                break;
-            case "9":
-                handler.UpdateStudent();
-                break;
-            case "10":
-                handler.GetStudentById();
-                break;
-            case "11":
-                handler.DeleteStudent();
-                break;
-            case "12":
-                handler.GetStudentsByAge();
-                break;
-            case "13":
-                handler.GetStudentsByGroupId();
-                break;
-            case "14":
-                handler.SearchGroupsByName();
-                break;
-            case "15":
-                handler.SearchStudents();
-                break;
-            case "16":
-                handler.GetAllGroupsWithStudents();
-                break;
+            try
+            {
+                switch (choice)
+                {
+                    case "0":
+                        exitRequested = true;
+                        Helpers.DisplayMessage("Program is stopping. Thank you!", ConsoleColor.Blue);
+                        break;
 
-            default:
-                Helpers.DisplayError("Invalid choice. Please use the menu.");
-                break;
+                    case "1":
+                        handler.CreateGroup();
+                        break;
+                    case "2":
+                        handler.UpdateGroup();
+                        break;
+                    case "3":
+                        handler.DeleteGroup();
+                        break;
+                    case "4":
+                        handler.GetGroupById();
+                        break;
+                    case "5":
+                        handler.GetGroupsByTeacher();
+                        break;
+                    case "6":
+                        handler.GetGroupsByRoom();
+                        break;
+                    case "7":
+                        handler.GetAllGroups();
+                        break;
+
+                    case "8":
+                        handler.CreateStudent();
+                        break;
+                    case "9":
+                        handler.UpdateStudent();
+                        break;
+                    case "10":
+                        handler.GetStudentById();
+                        break;
+                    case "11":
+                        handler.DeleteStudent();
+                        break;
+                    case "12":
+                        handler.GetStudentsByAge();
+                        break;
+                    case "13":
+                        handler.GetStudentsByGroupId();
+                        break;
+                    case "14":
+                        handler.SearchGroupsByName();
+                        break;
+                    case "15":
+                        handler.SearchStudents();
+                        break;
+                    case "16":
+                        handler.GetAllGroupsWithStudents();
+                        break;
+
+                    default:
+                        Helpers.DisplayError("Invalid choice. Please use the menu.");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Helpers.DisplayError(ex.Message);
+            }
+
+            if (!exitRequested)
+            {
+                Helpers.ReadInput("\nPress Enter to continue", allowEscape: false);
+                Console.Clear();
+            }
+
         }
-    }
-    catch (Exception ex)
-    {
-        Helpers.DisplayError(ex.Message);
-    }
 
-    if (!exitRequested)
-    {
-        Helpers.ReadInput("\nPress Enter to continue", allowEscape: false);
-        Console.Clear();
     }
-
 }
+
+
